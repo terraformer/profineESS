e62ef602f2290e50cec94039e9ea0c6b5db6cd65
Christian Liebl <christian.liebl@live.de>
2023-07-15 22:24:01 +0200

    to review on monday

--- app/src/main/java/com/profinegroup/profineess/MainActivity.java
+++ app/src/main/java/com/profinegroup/profineess/MainActivity.java
@@ -1,12 +1,9 @@
 package com.profinegroup.profineess;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,19 +12,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "Start");
-        openWebPage("https://mitarbeiter.profine-group.com");
+
+        openWebPage(URL);
         finish();
-        Log.d(TAG, "END");
+
     }
     private void openWebPage(String url) {
         Uri webpage = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-        Log.d(TAG, "preload");
+
         // Überprüfe, ob eine Activity den Intent behandeln kann, bevor sie gestartet wird.
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
-            Log.d(TAG, "load");
+
         }
     }
 }
